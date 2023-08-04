1e4b85ae42b6f270dd0cd63f99ab575ac222a5ce
Lafatra <lafatraravelojaona5@gmail.com>
2023-08-04 06:18:36 +0200

    first commit

---
+++ app/src/main/java/com/itumaster/madaventure/MainActivity.java
@@ -0,0 +1,18 @@
+package com.itumaster.madaventure;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button accueil, detail;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
